33eaf8d4b6a20b3ef0fe0dd9c1266d9bcc645f05
Brenzee <karelisbrendons@gmail.com>
2023-02-01 22:31:32 +0200

    Lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export * from './multicall';
-export * from './constants';
+export * from "./multicall"
+export * from "./constants"
--- src/test/foo.test.ts
+++ src/test/foo.test.ts
@@ -1,37 +1,39 @@
-import { Call, Multicall } from '../multicall';
-import * as dotenv from 'dotenv';
-import { ethers } from 'ethers';
+import { Call, Multicall } from "../multicall"
+import * as dotenv from "dotenv"
+import { ethers } from "ethers"
 
-dotenv.config();
+dotenv.config()
 
-const RPC_URL = process.env.MAINNET_RPC_URL || '';
+const RPC_URL = process.env.MAINNET_RPC_URL || ""
 
-describe('Test Multicall', () => {
-  it('Test', async () => {
+describe("Test Multicall", () => {
+  it("Test", async () => {
     const calls: Call[] = [
       {
-        target: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-        methodName: 'balanceOf',
+        target: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        methodName: "balanceOf",
         params: [
           {
-            type: 'address',
-            value: '0x0000000000000000000000000000000000000000',
+            type: "address",
+            value: "0x0000000000000000000000000000000000000000",
           },
         ],
         returnTypes: [
           {
-            type: 'uint256',
-            name: 'balance',
+            type: "uint256",
+            name: "balance",
           },
         ],
       },
-    ];
+    ]
 
     const multicall = new Multicall({
       provider: new ethers.providers.JsonRpcProvider(RPC_URL),
       chainId: 1,
-    });
+    })
 
-    const data = await multicall.getData(calls);
-  });
-});
+    const data = await multicall.getData(calls)
+
+    console.log(data)
+  })
+})
